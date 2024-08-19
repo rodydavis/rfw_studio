f476c34b5398da49a97bc3053dbab4b7a71ac082
rodydavis <rodydavis@users.noreply.github.com>
2024-08-19 13:33:36 +0000

    deploy: 653078f42d5082e8a17d3fd74a0944f488fcd49f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1344933580"
+    serviceWorkerVersion: "1509499340"
   }
 });
